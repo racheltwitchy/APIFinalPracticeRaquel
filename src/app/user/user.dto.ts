d3f6c2cc856559404cfb07fdf1fc9e7b4729978a
racheltwitchy <raquel.pelaire@gmail.com>
2025-01-10 19:28:28 +0100

    Users creation

--- src/app/user/user.dto.ts
+++ src/app/user/user.dto.ts
@@ -1,8 +1,13 @@
-export interface User {
-    id: string;
+export interface CreateUserDTO {
     name: string;
     email: string;
     role: 'patient' | 'doctor' | 'admin';
     specialties?: string[]; // Applicable only for doctors
+  }
+  
+  export interface UpdateUserDTO {
+    name?: string;
+    email?: string;
+    specialties?: string[];
   }
   
