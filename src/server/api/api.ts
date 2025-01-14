d9965f50f589a248137e468caa41245a0a884b5c
racheltwitchy <raquel.pelaire@gmail.com>
2025-01-14 23:13:55 +0100

    Add endpoint for specialty and department

--- src/server/api/api.ts
+++ src/server/api/api.ts
@@ -4,6 +4,8 @@ import { UserController } from "../../app/user/user.controller";
 import { AuditController } from "../../app/audit-logs/audit.controller";
 import { AppointmentController } from "../../app/appointment/appointment.controller";
 import { MedicalRecordController } from "../../app/medical-record/medical-record.controller";
+import { SpecialtyController } from "../../app/specialty/specialty.controller";
+import { DepartmentController } from "../../app/department/department.controller";
 
 @Service()
 export class Api {
@@ -13,7 +15,9 @@ export class Api {
     private userController: UserController,
     private auditController: AuditController,
     private appointmentController: AppointmentController,
-    private medicalRecordController: MedicalRecordController
+    private medicalRecordController: MedicalRecordController,
+    private specialtyController: SpecialtyController,
+    private departmentController: DepartmentController
     ) {
     this.router = Router();
     this.mountRoutes();
@@ -24,6 +28,8 @@ export class Api {
     this.router.use("/audit-logs", this.auditController.router);
     this.router.use("/appointments", this.appointmentController.router);
     this.router.use("/medical-records", this.medicalRecordController.router);
+    this.router.use("/specialties", this.specialtyController.router);
+    this.router.use("/departments", this.departmentController.router);
   }
 
   getRouter(): Router {
